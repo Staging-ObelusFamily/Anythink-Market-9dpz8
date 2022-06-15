efbc0917033a9e2160dc0e1d2f7558fd15f55983
nivrichter <nivrichter@gmail.com>
2022-06-15 18:02:36 +0300

    dsf

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -245,7 +245,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
     ) -> Item:
         item_row = await queries.get_item_by_slug(self.connection, slug=slug)
         if item_row:
-            return  await self._get_item_from_db_record(
+            return await self._get_item_from_db_record(
                 item_row=item_row,
                 slug=item_row[SLUG_ALIAS],
                 seller_username=item_row[SELLER_USERNAME_ALIAS],
